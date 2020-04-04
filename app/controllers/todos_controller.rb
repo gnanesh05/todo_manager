3944010862bf7411d6a5da0a34ee59417868aa22
gnanesh05 <gnanesh007.mk@gmail.com>
2020-04-04 21:20:55 +0530

    user can update a todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -29,6 +29,6 @@ class TodosController < ApplicationController
     todo = Todo.find(id)
     todo.completed = completed
     todo.save!
-    render plain: "the todo has been updated with #{completed}"
+    redirect_to todos_path
   end
 end
